525ed3dc180934e380475b452cb8c675b3a3a18a
Ivan Lieskov <ivan.lieskov@protonmail.com>
2022-09-27 21:43:03 +0200

    hardhat typescript config file

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    polygon: {
+      url: "https://rpc-mumbai.maticvigil.com" /*"https://polygon-rpc.com"*/,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
+};
+
+export default config;
